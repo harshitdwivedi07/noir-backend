016a8dc52d894f90b7acac1e6c1a2689866754b7
harshitdwivedi07 <dwivedih556@gmail.com>
2026-03-26 19:19:52 +0530

    fixed cors duplicate error

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app  = express();
 const PORT = process.env.PORT || 5000;
 
 // ── MIDDLEWARE ──
-app.use(cors());
+app.use(cors({
+  origin: "https://preeminent-cascaron-77b356.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // ── ROUTES ──
