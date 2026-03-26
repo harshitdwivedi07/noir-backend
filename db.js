0c7d271133fd38ff44f05318f2661aac9cfb1eca
harshitdwivedi07 <dwivedih556@gmail.com>
2026-03-26 19:38:26 +0530

    fixed cors + db

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host:     'localhost',
-  user:     'root',
-  password: '1234', // paste your actual password here
-  database: 'noir_fashion',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 const db = pool.promise();
 
-module.exports = db 
+// 🔥 Add this to test connection
+pool.getConnection((err, conn) => {
+  if (err) {
+    console.error("❌ DB ERROR:", err);
+  } else {
+    console.log("✅ MySQL Connected");
+    conn.release();
+  }
+});
+
+module.exports = db;
