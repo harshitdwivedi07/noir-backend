cd0fd08956f1eec8c0f31e20c25333e7c62da66b
harshitdwivedi07 <dwivedih556@gmail.com>
2026-03-26 18:56:12 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 // ── MIDDLEWARE ──
 app.use(cors({
-  origin: 'https://inspiring-blini-72c05c.netlify.app'
-}));         // allows frontend to talk to backend
+  origin: [
+    'https://preeminent-cascaron-77b356.netlify.app',
+    'https://inspiring-blini-72c05c.netlify.app'
+  ]
+}));       // allows frontend to talk to backend
 app.use(express.json());   // lets us read JSON from request body
 
 // ── ROUTES ──
